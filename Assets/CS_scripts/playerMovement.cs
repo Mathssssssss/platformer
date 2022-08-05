6bc065d30380154d2f059b28f3a192daeaee1d34
mathh <parfenmaffet@gmail.com>
2022-08-05 11:57:24 +0300

    Start

---
+++ Assets/CS_scripts/playerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpboost;
+    private Rigidbody2D linkToRigidbody;
+    private int countOfJumps = 1;
+    private int health {get;set;}
+    private void Movement(Rigidbody2D rigidbody){
+        float velocityOfPlayerX = Input.GetAxisRaw("Horizontal") * speed;
+        float velocityOfPlayerY = 0f;
+        if(Input.GetKeyDown("space") && countOfJumps > 0)
+        {
+            velocityOfPlayerY = jumpboost;
+        }
+        var playerDirection = new Vector2(velocityOfPlayerX,velocityOfPlayerY);
+        rigidbody.velocity = playerDirection;
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        linkToRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(linkToRigidbody);
+    }
+}
